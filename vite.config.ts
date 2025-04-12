1d034d5144d25bea70f1a02bc240e0129cb01845
Kamil Pawlak <ricoet22@gmail.com>
2025-04-12 12:37:35 +0200

    feat: init Vite PWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,23 @@ export default defineConfig({
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["apple-touch-icon.png", "icon.png"],
+      manifest: {
+        name: "Moje Roślinki",
+        short_name: "Moje Roślinki",
+        description: "Zarządzaj swoimi roślinami",
+        theme_color: "#E0C8B1",
+        icons: [
+          {
+            src: "icon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
