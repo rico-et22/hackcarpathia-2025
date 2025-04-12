62d0c812b676edbeced542387a0c0b5be267f6e1
Atai Ibragimov <aibragimov2003@bk.ru>
2025-04-12 16:02:14 +0200

    index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,31 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 
 
 export const Route = createFileRoute("/")({
-  component: Index,
+  component:() => (
+      <div className="max-w-[300px] mx-auto">
+        <img src="image.png" alt="" className="w-[50%] mx-auto block"/>
+  
+        <div className="text-center">
+          
+          <h1 className="mb-[50px]">BLOOM</h1>
+          <h2 >
+          Planuj swój ogród
+          </h2>
+        <h2 >
+        Oszczędzaj wodę
+        </h2>
+          
+        </div>
+  
+        <hr />
+        <Outlet />
+        <TanStackRouterDevtools />
+      </div>
+    ),
+  
 });
 
 
