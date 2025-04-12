1ee400653276732d3640634a0727bb4350dc16a5
Atai Ibragimov <aibragimov2003@bk.ru>
2025-04-12 16:06:10 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,31 +1,9 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { createFileRoute } from "@tanstack/react-router";
 
 
 
 export const Route = createFileRoute("/")({
-  component:() => (
-      <div className="max-w-[300px] mx-auto">
-        <img src="image.png" alt="" className="w-[50%] mx-auto block"/>
-  
-        <div className="text-center">
-          
-          <h1 className="mb-[50px]">BLOOM</h1>
-          <h2 >
-          Planuj swój ogród
-          </h2>
-        <h2 >
-        Oszczędzaj wodę
-        </h2>
-          
-        </div>
-  
-        <hr />
-        <Outlet />
-        <TanStackRouterDevtools />
-      </div>
-    ),
-  
+  component: Index,
 });
 
 
