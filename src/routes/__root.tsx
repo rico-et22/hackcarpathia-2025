5d2625f0667dbea38585e2f2a756990d0fb7633f
Atai Ibragimov <aibragimov2003@bk.ru>
2025-04-12 13:24:49 +0200

    home removed

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,12 +4,7 @@ import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 export const Route = createRootRoute({
   component: () => (
     <div className="max-w-[500px] mx-auto">
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
       
-      </div>
       <hr />
       <Outlet />
       <TanStackRouterDevtools />
